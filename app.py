50f2907136cb247e1e2b735fc191f6f325d49e71
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2022-09-13 08:04:08 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,12 +4,22 @@ activate_this = '/var/www/flask/a/venv/bin/activate_this.py'
 with open(activate_this) as file:
    exec(file.read(), dict(__file__=activate_this))
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/') 
 def hello_world():
    return 'Hello world from Flask!' 
+
+
+@app.route('/hello') 
+def hello():
+   return render_template('home.html')
+
+
+@app.route('/html') 
+def index():
+   return render_template('index.html')
 
 #if __name__ == '__main__':
 #      app.run()
